9ac9132d41b43b400083c483d20c838aca667257
JNeidel <jneidel@outlook.com>
2017-06-25 18:47:59 +0200

    Backend functionality to display the api data

--- weather.js
+++ weather.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
 	var latitude;
 	var longitude;
+	toggle_fahrenheit = false;
 
 	function getLocation (callback) {
 		navigator.geolocation.getCurrentPosition(function(position) {
@@ -23,10 +24,53 @@ $(document).ready(function() {
 			dataType: "jsonp",
 			async: false,
 			success: function (data) {
-				temperature = data.currently.apparentTemperature;
-				readable = data.currently.summary;
-				icon = data.currently.icon;
-				console.log(data);
+				temp_in_f = Math.round(data.currently.apparentTemperature);
+				temp_in_c = Math.round((data.currently.apparentTemperature - 32) / (9 / 5));
+
+				if (toggle_fahrenheit) { //Display temp in f/c
+					$("#temp").text(String(temp_in_f) + " °f".slice(1));
+				} else {
+					$("#temp").html(String(temp_in_c) + "°c".slice(1));
+				}
+
+				$("#place").text(data.timezone.split("/")[1]); //Display location
+				$("#weather").text(data.currently.summary); //Readable weather
+
+				switch (data.currently.icon) { //Choose icon to display
+					case ("clear-day"):
+						$("#icon").attr("src", "pictures/weather clear-day.png");
+						break;
+					case ("clear-night"):
+						$("#icon").attr("src", "pictures/weather clear-night.png");
+						break;
+					case ("rain"):
+						$("#icon").attr("src", "pictures/weather rain.png");
+						break;
+					case ("snow"):
+						$("#icon").attr("src", "pictures/weather snow.png");
+						break;
+					case ("sleet"):
+						$("#icon").attr("src", "pictures/weather sleet.png");
+						break;
+					case ("wind"):
+						$("#icon").attr("src", "pictures/weather wind.png");
+						break;
+					case ("fog"):
+						$("#icon").attr("src", "pictures/weather fog.png");
+						break;
+					case ("cloudy"):
+						$("#icon").attr("src", "pictures/weather cloudy.png");
+						break;
+					case ("partly-cloudy-day"):
+						$("#icon").attr("src", "pictures/weather partly-cloudy-day.png");
+						break;
+					case ("partly-cloudy-night"):
+						$("#icon").attr("src", "pictures/weather partly-cloudy-night.png");
+						break;
+					default:
+						$("#icon").attr("src", "pictures/weather cloudy.png");
+						break;
+				};
 			}
 		});
 	}
@@ -34,20 +78,6 @@ $(document).ready(function() {
 	getLocation(returnLocation);
 
 	/*
-	Icon possibilities:
-	clear-day, 
-	clear-night, 
-	rain, 
-	snow, 
-	sleet, 
-	wind, 
-	fog, 
-	cloudy, 
-	partly-cloudy-day,
-	partly-cloudy-night
-
-	ensure that a default is defined
-
 	https://darksky.net/dev/docs/response
 	*/
 });
