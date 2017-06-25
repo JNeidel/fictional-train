7ffe0ef7c6519c6ee6dfd92c2010ebc7518d0c8a
JNeidel <jneidel@outlook.com>
2017-06-25 14:34:15 +0200

    Added weather api call

--- weather.js
+++ weather.js
@@ -13,7 +13,25 @@ $(document).ready(function() {
 	function returnLocation (lat, lon) {
 		latitude = lat;
 		longitude = lon;
+		getWeather(lon, lat);
+	}
+
+	function getWeather (lon, lat) {
+		url = "https://api.darksky.net/forecast/2fd9c6e3bd55ef3fb02da8d9d01ae78b/" + String(lat) + "," + String(lon);
+		$.ajax(url, {
+			url: url,
+			dataType: "jsonp",
+			async: false,
+			success: function (data) {
+				temperature = data.currently.apparentTemperature;
+				readable = data.currently.summary;
+				icon = data.currently.icon;
+				console.log(data);
+			}
+		});
 	}
 
 	getLocation(returnLocation);
+
+	
 });
