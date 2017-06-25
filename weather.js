d7d8d94003a03eeb1d3e7486105222c9bbe1bcea
JNeidel <jneidel@outlook.com>
2017-06-25 14:33:14 +0200

    Added location call

--- weather.js
+++ weather.js
@@ -1,1 +1,19 @@
+$(document).ready(function() {
 
+	var latitude;
+	var longitude;
+
+	function getLocation (callback) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			lat = position.coords.latitude;
+      		lon = position.coords.longitude;
+			callback(lat, lon);
+		});
+	}
+	function returnLocation (lat, lon) {
+		latitude = lat;
+		longitude = lon;
+	}
+
+	getLocation(returnLocation);
+});
