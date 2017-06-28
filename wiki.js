588df4840322d37f4b6f0f2e1d7f02983ffcd7a4
JNeidel <jneidel@outlook.com>
2017-06-28 12:03:05 +0200

    Add basic api call for exactly corret query

--- wiki.js
+++ wiki.js
@@ -1,1 +1,25 @@
+$(document).ready(function() {
 
+	input = "Monstercat";
+
+	function getList (input) {
+		url = "https://en.wikipedia.org/w/api.php?action=query&titles=" + input.split(" ").join("%20") + "&prop=revisions&rvprop=content&format=json"
+		$.ajax(url, {
+			url: url,
+			dataType: "jsonp",
+			async: false,
+			success: function (data) {
+				for (key in data.query.pages) {}
+				try {
+					var category = data.query.pages[key].revisions[0]["*"].split("{{Infobox ")[1].split("}}")[0].split("|")[0].split(" <!--")[0];
+					console.log(category);
+				} catch(err) {
+					console.log(data.query.pages[key].revisions[0]["*"]);
+					console.log(data.query.pages);
+				}
+			}
+		});
+	}
+
+	getList(input);
+});
