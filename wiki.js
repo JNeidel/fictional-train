5c8e21fcd9b93af531fa6abf5354b4b5660e612e
JNeidel <jneidel@outlook.com>
2017-07-03 16:31:07 +0200

    Switch to opensearch api

--- wiki.js
+++ wiki.js
@@ -3,23 +3,26 @@ $(document).ready(function() {
 	input = "Monstercat";
 
 	function getList (input) {
-		url = "https://en.wikipedia.org/w/api.php?action=query&titles=" + input.split(" ").join("%20") + "&prop=revisions&rvprop=content&format=json"
+		url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + input.split(" ").join("%20") + "&limit=4&namespace=0&format=json"
 		$.ajax(url, {
 			url: url,
 			dataType: "jsonp",
 			async: false,
 			success: function (data) {
-				for (key in data.query.pages) {}
-				try {
-					var category = data.query.pages[key].revisions[0]["*"].split("{{Infobox ")[1].split("}}")[0].split("|")[0].split(" <!--")[0];
-					console.log(category);
-				} catch(err) {
-					console.log(data.query.pages[key].revisions[0]["*"]);
-					console.log(data.query.pages);
+				console.log(data);
+				for (i = 0; i < data[1].length; i++) {
+					$("#results").append("<a href='" + data[3][i] + "' id='results-" + String(i) + "' target='_blank'></a>");
+					$("#results-" + String(i)).append("<p>" + data[1][i] + "</p>");
+					$("#results-" + String(i)).append("<p>" + data[2][i].slice(0, 140) + "</p>");
+					$("#results-" + String(i)).css({"text-decoration": "none", "color": "black"});
+					$("#results-" + String(i)).hover(function() {
+						$(this).css("background-color", "red");
+					}, function() {
+						$(this).css("background-color", "#fff");
+					});
 				}
+				
 			}
 		});
 	}
-
-	getList(input);
 });
